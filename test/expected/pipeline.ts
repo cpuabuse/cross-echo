e8f6cbc2c5300063b6d2bc9d62d140658714a3fa
Filipe Oliveira <52853539+oliverjiu@users.noreply.github.com>
2020-04-17 16:23:35 +0900

    Change: Documentation

--- test/expected/pipeline.ts
+++ test/expected/pipeline.ts
@@ -15,7 +15,7 @@ export const path: string = "C:\\";
 /**
  * A list of animals with backspaces between them.
  */
-export const animals: string = `Lion\u0008Tiger\u0008Dog\u0008`;
+export const animals: string = "Lion\u0008Tiger\u0008Dog\u0008";
 
 /**
  * A sequence of rings with the alert character.
@@ -27,17 +27,17 @@ Ring\u0007`;
 /**
  * A list of planets with carriage returns.
  */
-export const planets: string = `Earth\u000DVenus\u000DJupiter\u000D`;
+export const planets: string = "Earth\u000DVenus\u000DJupiter\u000D";
 
 /**
  * A list of snakes with the command escaped.
  */
-export const snakes: string = `Ball python\u001B00Anaconda`;
+export const snakes: string = "Ball python\u001B00Anaconda";
 
 /**
  * A list of countries with form feed.
  */
-export const countries: string = `Colombia\u000CMadagascar\u000CEgypt`;
+export const countries: string = "Colombia\u000CMadagascar\u000CEgypt";
 
 /**
  * Fruit table with tabs.
@@ -49,22 +49,22 @@ Apple	250	Red`;
 /**
  * List of beverages with vertical tabs.
  */
-export const beverages: string = `Whiskey\u000BVodka\u000BCola`;
+export const beverages: string = "Whiskey\u000BVodka\u000BCola";
 
 /**
  * List of Junk foods with End of text character.
  */
-export const junkFood: string = `Hot-dog\u0003Hamburguer\u0003Pizza`;
+export const junkFood: string = "Hot-dog\u0003Hamburguer\u0003Pizza";
 
 /**
  * List of martial-arts with newline character.
  */
-export const martialArts: string = `Karate\u000AJiu-Jitsu\u000ABoxe`;
+export const martialArts: string = "Karate\u000AJiu-Jitsu\u000ABoxe";
 
 /**
  * A list of electronics devices with alert character.
  */
-export const electronics: string = `computer\\afridge\\a`;
+export const electronics: string = "computer\\afridge\\a";
 
 /**
  * A list of cities with carriage returns.
